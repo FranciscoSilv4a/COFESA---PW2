f4aff140ebeb3364994229543a75c40543d2cb2f
FranciscoSilv4a <franciscofelipedasilva457@gmail.com>
2025-05-24 18:41:32 -0300

    Update and rename mercado.html to mercado.php
    mudei o arquivo para .php e mudei o consertei o caminho do link para o CSS.

--- Mercado/mercado.php
+++ Mercado/mercado.php
@@ -1,9 +1,12 @@
+<?php
+//
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="mercado.css">
+    <link rel="stylesheet" href="css/mercado.css">
     <link rel="icon" href="Imagens/cofesa icon.png">
     <title>Cofesa</title>
 </head>
